dde1f2707e5fec336d48912bb1d4f6f21e2acc5e
Guti <gutifer666@gmail.com>
2024-08-23 21:21:02 +0200

    added create test component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {TestComponent} from "./core/pages/test/test.component";
+import {CreateTestComponent} from "./core/pages/create-test/create-test.component";
 
 export const routes: Routes = [
-  { path: '', component: TestComponent },
+  { path: '', component: CreateTestComponent },
+  { path: 'test', component: TestComponent }
 ];
